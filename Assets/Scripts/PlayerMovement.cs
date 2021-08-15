43593343a71f30d655fc21d7cb9c9fa50cc63091
Anton1636 <hobermac228@gmil.com>
2021-08-15 16:49:53 +0300

    Add Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 chenge;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chenge = Vector3.zero;
+        chenge.x = Input.GetAxisRaw("Horizontal");
+        chenge.y = Input.GetAxisRaw("Vertical");
+
+        if(chenge != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + chenge * speed * Time.deltaTime);
+    }
+}
