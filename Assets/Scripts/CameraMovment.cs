11326733b87a374a0ac1c35f2ae2cad403208d8c
Anton1636 <hobermac228@gmil.com>
2021-08-15 19:03:04 +0300

    Add camera movment

---
+++ Assets/Scripts/CameraMovment.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+    private Transform target;
+    public float smothing;
+    public Vector2 maxPos;
+    public Vector2 minPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, minPos.x, maxPos.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, smothing);
+
+
+        }
+    }
+}
